c51edd5d841b5de778206e107b8eb730930d02af
MELEK <100144640+MELEK-GRL@users.noreply.github.com>
2025-06-03 13:44:19 +0300

    Add all project files

---
+++ App.tsx
@@ -0,0 +1,32 @@
+
+import React from 'react';
+
+import {
+
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+
+
+
+  return (
+    <View style={styles.sectionContainer}>
+ <Text>CHAT HOŞGELDİN</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+
+});
+
+export default App;
